fd82a7247c6e62335f68dbcb045c1ece327213c1
Volodymyr F <you@example.com>
2023-04-20 22:08:21 +0300

    stat_building_simple_site

---
+++ src/flask_app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+from src.email.messages import get_all_emails
+
+app = Flask(__name__)
+
+@app.route('/emails')
+def index():
+    return render_template('index.html', addresses=get_all_emails())
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
