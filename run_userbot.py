144a88a07c04ddd6d07ebf1ffc330f0fad41cc60
Volodymyr F <you@example.com>
2023-05-06 08:57:54 +0300

    fix

--- run_userbot.py
+++ run_userbot.py
@@ -14,7 +14,10 @@ userbot_app = Client("sessions/ai_bot", API_ID, API_HASH)
 
 @userbot_app.on_message(filters.text & filters.private)
 async def echo(client, message: types.Message):
-
+    if message.from_user.is_bot:
+        from loguru import logger
+        logger.info("this is a bot")
+        return 
     if prod:
         corrected_text = await correct_text(message.text)
     else:
