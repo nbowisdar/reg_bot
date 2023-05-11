73cb503266eb345694e297fa45109e660a8f9087
Volodymyr F <you@example.com>
2023-05-11 14:42:15 +0300

    fix

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from src.database.tables import EmailMessage, Email
 if __name__ == '__main__':
     c = 0
     for email in Email.select():
-        if email.sex == "❓":
+        if email.sex != "male" and email.sex != "female":
             email.sex = "male"
             email.save()
             c += 1
