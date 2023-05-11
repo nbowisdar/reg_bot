aa08e315d70bd50d7c60fd8bbaae8195becc179f
Volodymyr F <you@example.com>
2023-05-11 13:43:15 +0300

    mysql

---
+++ src/database/from_sqlite_to_mysql.py
@@ -0,0 +1,39 @@
+import sqlite3
+import mysql.connector
+
+# Connect to SQLite database
+sqlite_conn = sqlite3.connect('app.db')
+sqlite_cursor = sqlite_conn.cursor()
+
+# Connect to MySQL database
+mysql_conn = mysql.connector.connect(
+    host='localhost',
+    user='admin',
+    password='admin',
+    database='db'
+)
+mysql_cursor = mysql_conn.cursor()
+
+# Get list of tables in SQLite database
+sqlite_cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables = [table[0] for table in sqlite_cursor.fetchall()]
+
+# Transfer data from SQLite to MySQL for each table
+for table in tables:
+    # Get data from SQLite table
+    sqlite_cursor.execute(f"SELECT * FROM {table};")
+    data = sqlite_cursor.fetchall()
+
+    # Create MySQL table with same schema as SQLite table
+    sqlite_cursor.execute(f"PRAGMA table_info({table});")
+    schema = sqlite_cursor.fetchall()
+    mysql_cursor.execute(f"CREATE TABLE IF NOT EXISTS {table} ({','.join([f'{col[1]} {col[2]}' for col in schema])});")
+
+    # Insert data into MySQL table
+    for row in data:
+        mysql_cursor.execute(f"INSERT INTO {table} VALUES ({','.join(['%s']*len(row))})", row)
+
+# Commit changes and close connections
+mysql_conn.commit()
+mysql_conn.close()
+sqlite_conn.close()
